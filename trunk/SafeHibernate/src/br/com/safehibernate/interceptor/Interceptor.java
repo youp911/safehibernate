6ade30d77fc6d1d3abefe93fff5f220d94ad5813
jeancp <jeancp@616e2af1-bf2f-0410-8b06-834620bd8ec5>
2007-08-18 00:38:42 +0000

    Implementation

--- trunk/SafeHibernate/src/br/com/safehibernate/interceptor/Interceptor.java
+++ trunk/SafeHibernate/src/br/com/safehibernate/interceptor/Interceptor.java
@@ -2,6 +2,8 @@ package br.com.safehibernate.interceptor;
 
 import java.io.Serializable;
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 import org.hibernate.EmptyInterceptor;
 import org.hibernate.type.Type;
@@ -24,10 +26,12 @@ public class Interceptor extends EmptyInterceptor {
 		for (int i = 0; i < propertyNames.length; i++) {
 			String propertyName = propertyNames[i];
 			try {
-				Field field = clazz.getField(propertyName);
+				Field field = clazz.getDeclaredField(propertyName);
 				SafeField annotation = field.getAnnotation(SafeField.class);
 				if (annotation != null) {
-					field.set(entity, "BLABLABLA");
+					field.setAccessible(true);
+					Object value = field.get(entity);
+					field.set(entity, DataTransformer.encrypt(value));
 				}
 			} catch (SecurityException e) {
 				throw new RuntimeException(e);
@@ -43,5 +47,42 @@ public class Interceptor extends EmptyInterceptor {
 		return super.onSave(entity, id, state, propertyNames, types);
 	}
 	
-	
+	@Override
+	public boolean onLoad(Object entity,
+		Serializable id,
+		Object[] state,
+		String[] propertyNames,
+		Type[] types) {
+		boolean load = super.onLoad(entity, id, state, propertyNames, types);
+		
+		Class<?> clazz = entity.getClass();
+		for (int i = 0; i < propertyNames.length; i++) {
+			String propertyName = propertyNames[i];
+			try {
+				Field field = clazz.getDeclaredField(propertyName);
+				field.setAccessible(true);
+				SafeField annotation = field.getAnnotation(SafeField.class);
+				if (annotation != null) {
+					String methodName = "get" + propertyName.substring(0,1).toUpperCase() + propertyName.substring(1);
+					Method m = clazz.getMethod(methodName);
+					Object value = m.invoke(entity);
+					field.set(entity, DataTransformer.encrypt(value));
+				}
+			} catch (SecurityException e) {
+				throw new RuntimeException(e);
+			} catch (NoSuchFieldException e) {
+				throw new RuntimeException(e);
+			} catch (IllegalArgumentException e) {
+				throw new RuntimeException(e);
+			} catch (IllegalAccessException e) {
+				throw new RuntimeException(e);
+			} catch (NoSuchMethodException e) {
+				throw new RuntimeException(e);
+			} catch (InvocationTargetException e) {
+				throw new RuntimeException(e);
+			} 
+		}
+		
+		return load;
+	}
 }
