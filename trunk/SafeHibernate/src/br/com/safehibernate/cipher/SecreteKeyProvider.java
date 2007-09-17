bd7dc198e37bd58e79ff188a3afb07023d32acda
jeancp <jeancp@616e2af1-bf2f-0410-8b06-834620bd8ec5>
2007-09-17 02:05:07 +0000

    RSAProvider working

--- trunk/SafeHibernate/src/br/com/safehibernate/cipher/SecreteKeyProvider.java
+++
@@ -1,9 +0,0 @@
-package br.com.safehibernate.provider;
-
-import javax.crypto.SecretKey;
-
-public abstract class SecreteKeyProvider implements ICertificateProvider {
-
-	public abstract SecretKey getSecreteKey();
-
-}
