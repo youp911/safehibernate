a6e6c1398f82bb5e51ed9b4a4512336d802b57d8
jeancp <jeancp@616e2af1-bf2f-0410-8b06-834620bd8ec5>
2007-10-10 02:36:11 +0000

    Implementation

--- trunk/SafeHibernateTest/src/provider/ProviderSuite.java
+++ trunk/SafeHibernateTest/src/provider/ProviderSuite.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(value=Suite.class)
-@SuiteClasses(value={TestAESProvider.class, TestDESProvider.class, TestRSAProvider.class, TestSignatureProvider.class})
+@SuiteClasses(value={TestDataTransformer.class, TestAESProvider.class, TestDESProvider.class, TestRSAProvider.class, TestSignatureProvider.class})
 public class ProviderSuite {
 
 	
